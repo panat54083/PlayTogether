caf12aef6b86cfde21eb95bce1f5da94f3c9fdde
Panat <panat54083@gmail.com>
2022-06-27 20:02:02 +0700

    add window capture code

---
+++ auto_fishing.py
@@ -0,0 +1,36 @@
+#ref = https://youtu.be/WymCpVUPWQ4
+import numpy as np
+import pyautogui as pg
+import cv2 as cv
+import os
+import time
+from windowcapture import WindowCapture
+
+window_name = "LDPlayer"
+wincap = WindowCapture(window_name)
+
+
+
+loop_time = time.time()
+while(True):
+
+    screenshot = wincap.get_screenshot()
+    # # we need to do this if we use 3rd party way to capture window screenshot
+    # # we need to convert screenshot format to input that open cv understand.
+    # screenshot = np.array(screenshot)
+    # # Convert RGB to BGR 
+    # screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+  
+    cv.imshow('Computer Vision', screenshot)
+
+    print("FPS {}".format(1 / (time.time() - loop_time)))
+    loop_time = time.time()
+
+
+    # press "q" with the output windows focused to exist.
+    
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print("Done...")
