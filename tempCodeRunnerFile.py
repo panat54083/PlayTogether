9d22138466e6628197065dbdfe83327357ba1b00
Panat <panat54083@gmail.com>
2022-06-29 23:22:43 +0700

    detect muti-exclamation point

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
--- vision.py
+++ vision.py
@@ -1,5 +1,6 @@
 from turtle import window_width
 import cv2 as cv
+from cv2 import line
 import numpy as np
 
 # imread Methods
@@ -9,30 +10,41 @@ exclamation_img = cv.imread('picture/exclamation point2.PNG', cv.IMREAD_GRAYSCAL
 
 result = cv.matchTemplate(playtogeter_img, exclamation_img, cv.TM_CCOEFF_NORMED)
 
-# get the best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+# print(result)
 
-print('Best match top left poistion: %s' % str(max_loc))
-print('Best match confidence: %s' % str(max_val))
+threshold = 0.90
+locations = np.where(result >= threshold)
+# print(locations)
 
-threshold = 0.3
-if max_val >= threshold:
+# we can zip those up into position tuples 
+# list[start:stop:step] *list == unpack lists
+locations = list(zip(*locations[::-1]))
+print(locations)
+
+if locations:
     print('Found..')
 
-    # get dimentions of the exclamation point image
-    exclamation_w = exclamation_img.shape[1]
-    exclamation_h = exclamation_img.shape[0]
-    
-    top_left = max_loc
-    bottom_right = (top_left[0] + exclamation_w, top_left[1] + exclamation_h)
-    # print(bottom_right)
-    
-    cv.rectangle(playtogeter_img, top_left, bottom_right,
-                    color = (0,0,255), thickness = 2, lineType=cv.LINE_4)
-    cv.imshow('Result', playtogeter_img)                
+    ecm_w = exclamation_img.shape[1]
+    ecm_h = exclamation_img.shape[0]
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
+
+    # need to loop over all the locations and draw their rectangle
+    for loc in locations:
+        # determine the box positions
+        top_left = loc
+        print(top_left)
+        bottom_right = (top_left[0] + ecm_w, top_left[1] + ecm_h)
+        print(bottom_right)
+
+        # draw the box
+        cv.rectangle(playtogeter_img, top_left, bottom_right, 
+                    line_color, line_type)
+
+    cv.imshow('Matches', playtogeter_img)
     cv.waitKey()
+
 else:
-    print('Not found..')
-
-
+    print("Not found..")
+    
 
