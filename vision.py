d7f1fc4eafc8e93f7d04159db8194d33efcb7edd
Panat <panat54083@gmail.com>
2022-06-30 09:28:59 +0700

    create list of rectangles

--- vision.py
+++ vision.py
@@ -1,6 +1,7 @@
 from turtle import window_width
 import cv2 as cv
 from cv2 import line
+from cv2 import rectangle
 import numpy as np
 
 # imread Methods
@@ -8,35 +9,40 @@ import numpy as np
 playtogeter_img = cv.imread('picture/test2.PNG', cv.IMREAD_GRAYSCALE)
 exclamation_img = cv.imread('picture/exclamation point2.PNG', cv.IMREAD_GRAYSCALE)
 
+ecm_w = exclamation_img.shape[1]
+ecm_h = exclamation_img.shape[0]
+
 result = cv.matchTemplate(playtogeter_img, exclamation_img, cv.TM_CCOEFF_NORMED)
 
 # print(result)
 
 threshold = 0.90
 locations = np.where(result >= threshold)
-# print(locations)
-
 # we can zip those up into position tuples 
 # list[start:stop:step] *list == unpack lists
 locations = list(zip(*locations[::-1]))
 print(locations)
 
-if locations:
+# create the list of [x, y, w, h] rectangles
+rectangles = []
+for loc in locations:
+    rect = [int(loc[0]), int(loc[1]), ecm_w, ecm_h]
+    rectangles.append(rect)
+
+print(rectangles)
+
+if len(rectangles):
     print('Found..')
 
-    ecm_w = exclamation_img.shape[1]
-    ecm_h = exclamation_img.shape[0]
+
     line_color = (0, 255, 0)
     line_type = cv.LINE_4
 
     # need to loop over all the locations and draw their rectangle
-    for loc in locations:
+    for (x, y, w, h) in rectangles:
         # determine the box positions
-        top_left = loc
-        print(top_left)
-        bottom_right = (top_left[0] + ecm_w, top_left[1] + ecm_h)
-        print(bottom_right)
-
+        top_left = (x, y)
+        bottom_right = (x + w, y + h)
         # draw the box
         cv.rectangle(playtogeter_img, top_left, bottom_right, 
                     line_color, line_type)
