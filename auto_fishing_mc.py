b363a51a53eb9f7744c2148fcff1b60b8b3d6bb9
Panat <panat54083@gmail.com>
2022-07-05 00:18:11 +0700

    initiate for machine learning

---
+++ auto_fishing_mc.py
@@ -0,0 +1,30 @@
+#ref = https://youtu.be/WymCpVUPWQ4
+from cv2 import rectangle
+import numpy as np
+import pyautogui as pg
+import cv2 as cv
+import os
+import time
+from windowcapture import WindowCapture
+
+window_name = "ApowerMirror Livestream"
+# window_name = None
+wincap = WindowCapture(window_name)
+
+loop_time = time.time()
+while(True):
+
+    screenshot = wincap.get_screenshot()
+    
+    cv.imshow('Computer Vision', screenshot)
+
+    print("FPS {}".format(1 / (time.time() - loop_time)))
+    loop_time = time.time()
+
+    # press "q" with the output windows focused to exist.
+    
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print("Done...")
