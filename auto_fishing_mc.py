8b37a1352c5491c3922a1829b0cf3f85397e5e1d
Panat <panat54083@gmail.com>
2022-07-05 00:31:17 +0700

    add key for detecting pos or neg pictures

--- auto_fishing_mc.py
+++ auto_fishing_mc.py
@@ -20,11 +20,16 @@ while(True):
 
     print("FPS {}".format(1 / (time.time() - loop_time)))
     loop_time = time.time()
-
+    key = cv.waitKey(1)
     # press "q" with the output windows focused to exist.
-    
-    if cv.waitKey(1) == ord('q'):
+    if key == ord('q'):
         cv.destroyAllWindows()
         break
+    elif key == ord('f'):
+        cv.imwrite('positive/{}.jpg'.format(loop_time), screenshot)
+    elif key == ord('d'):
+        cv.imwrite('negative/{}.jpg'.format(loop_time), screenshot)
+        
+
 
 print("Done...")
