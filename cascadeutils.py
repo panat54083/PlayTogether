d091895bc83f18218d256b88669aa937a52ada09
Panat <panat54083@gmail.com>
2022-07-05 23:32:09 +0700

    train cascade

--- cascadeutils.py
+++ cascadeutils.py
@@ -14,6 +14,8 @@ if "__main__" == __name__:
 
 # D:/Programing/opencv-3.4.11/opencv/build/x64/vc15/bin/opencv_annotation.exe --annotations=pos.txt --images=positive/
 # D:/Programing/opencv-3.4.11/opencv/build/x64/vc15/bin/opencv_createsamples.exe -info pos.txt -w 24 -h 24 -num 1000 -vec pos.vec
+# D:/Programing/opencv-3.4.11/opencv/build/x64/vc15/bin/opencv_traincascade.exe -data cascade/ -vec pos.vec -bg neg.txt -w 24 -h 24 -numPos 30 -numNeg 15 -numStages 5 
+
 # press "c" to conferm
 # press "d" to undo
 # press "esc" to exit
