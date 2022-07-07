7b1ee82fb5453a642da920516d96cb4a1d533e2e
Panat <panat54083@gmail.com>
2022-07-07 19:48:29 +0700

    add click in main

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 #ref = https://youtu.be/WymCpVUPWQ4
-from cv2 import rectangle
-import numpy as np
+from numpy import rec
 import pyautogui as pg
 import cv2 as cv
-import os
 import time
 from windowcapture import WindowCapture
 from vision import Vision
-from hsvfilter import HsvFilter
-from edgefilter import EdgeFilter
-
+from edgefilter import EdgeFilter 
+import pyautogui
 window_name = "ApowerMirror Livestream"
 # window_name = None
 wincap = WindowCapture(window_name)
-
 vision_exclamation = Vision('picture/edge exclam.jpg')
-# vision_exclamation = Vision('picture/IMG_1545.jpg')    
 vision_exclamation.init_control_gui()
 loop_time = time.time()
 while(True):
@@ -23,32 +18,30 @@ while(True):
     screenshot = wincap.get_screenshot()
     # crop screen shot for detecting only exclamation point
     crop_screenshot = screenshot
-    x, w, y, h = [550, 400, 100, 200]
+    x, w, y, h = [650, 300, 100, 200]
     crop_screenshot = crop_screenshot[y:y+h, x:x+w]
-
-    # pre-process the image
-    # hsvfilter = HsvFilter(18, 0, 255, 58, 46, 255, 0, 0, 0, 0)
-    edgefilter = EdgeFilter(8, 1, 1, 100, 200)
-    pre_process_img = vision_exclamation.apply_hsv_filter(screenshot)
-
-    edge_process_img = vision_exclamation.apply_edge_filter(crop_screenshot, None)
-    # screenshot = cv.cvtColor(screenshot, cv.COLOR_RGBA2GRAY)
     # do object detection
-    rectangles = vision_exclamation.find(edge_process_img, 0.50)
-
+    edgefilter = EdgeFilter(8, 1, 1, 100, 200)
+    edge_process_img = vision_exclamation.apply_edge_filter(crop_screenshot, None)
+    rectangles = vision_exclamation.find(edge_process_img, 0.30)
     output_img = vision_exclamation.draw_rectangles(edge_process_img, rectangles)
-
-
+    
     # display the processed image
-    cv.imshow('HSV filter', pre_process_img)
-    cv.imshow('Edge filter', output_img)
-    # cv.imshow('Computer Vision', screenshot)
+    cv.imshow('Computer Vision', output_img)
+
+    # take bot actions
+    if len(rectangles) > 0:
+        # to click
+        # targets = pyautogui.move(1495,631)
+        # target = wincap.get_screen_position(targets[0])
+        pyautogui.click(x=1582, y=751)
+        # time.sleep(1.5)
+        # pyautogui.click(x=1482, y=751)
 
     print("FPS {}".format(1 / (time.time() - loop_time)))
     loop_time = time.time()
 
     # press "q" with the output windows focused to exist.
-    
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()
         break
