dcfdf44ad61895d74911065ea71e29bf344d743f
Panat <panat54083@gmail.com>
2022-07-09 16:23:28 +0700

    take detection

--- main.py
+++ main.py
@@ -10,33 +10,56 @@ import pyautogui
 window_name = "ApowerMirror Livestream"
 # window_name = None
 wincap = WindowCapture(window_name)
+win_auto = pyautogui.getWindowsWithTitle(window_name)[0]
+x_win = win_auto.topleft[0]
+y_win = win_auto.topleft[1]
+
 vision_exclamation = Vision('picture/edge exclam.jpg')
+vision_take = Vision('picture/take.jpg')
+
 vision_exclamation.init_control_gui()
 loop_time = time.time()
 while(True):
 
     screenshot = wincap.get_screenshot()
     # crop screen shot for detecting only exclamation point
-    crop_screenshot = screenshot
     x, w, y, h = [650, 300, 100, 200]
-    crop_screenshot = crop_screenshot[y:y+h, x:x+w]
+    crop_screenshot1 = screenshot[y:y+h, x:x+w]
     # do object detection
     edgefilter = EdgeFilter(8, 1, 1, 100, 200)
-    edge_process_img = vision_exclamation.apply_edge_filter(crop_screenshot, None)
-    rectangles = vision_exclamation.find(edge_process_img, 0.30)
-    output_img = vision_exclamation.draw_rectangles(edge_process_img, rectangles)
-    
+    edge_process_img = vision_exclamation.apply_edge_filter(crop_screenshot1)
+    exclamation_rectangles = vision_exclamation.find(edge_process_img, 0.30)
+    output_img = vision_exclamation.draw_rectangles(edge_process_img, exclamation_rectangles)
+
+    # take detector and poistion
+    x, w, y, h = [1000, 500, 500, 100]
+    crop_screenshot2 = screenshot[y:y+h, x:x+w]
+
+    take_rectangles = vision_take.find(crop_screenshot2, 0.5)
+    take_detector = vision_take.draw_rectangles(crop_screenshot2, take_rectangles)
+
     # display the processed image
-    cv.imshow('Computer Vision', output_img)
+    cv.imshow('Computer Vision1', output_img)
+    cv.imshow('Computer Vision2', take_detector)
 
     # take bot actions
-    if len(rectangles) > 0:
+    if len(exclamation_rectangles) > 0:
         # to click
         # targets = pyautogui.move(1495,631)
         # target = wincap.get_screen_position(targets[0])
-        pyautogui.click(x=1582, y=751)
+        pyautogui.click(x=1550, y=751)
         # time.sleep(1.5)
         # pyautogui.click(x=1482, y=751)
+    if len(take_rectangles) > 0:
+        # https://pyautogui.readthedocs.io/en/latest/roadmap.html
+        
+        points = vision_take.get_click_points(take_rectangles)
+        x_point = points[0][0]
+        y_point = points[0][1]
+
+        
+        pyautogui.click(x= x_point+x_win+1000, y=y_point+y_win+500)
+        time.sleep(1)
 
     print("FPS {}".format(1 / (time.time() - loop_time)))
     loop_time = time.time()
